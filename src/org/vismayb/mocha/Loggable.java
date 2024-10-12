e34b4d98c2b64954463a88751e4ba1333e7308a1
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2024-10-12 12:34:03 -0700

    (doc) Add implementation note to Loggable

--- src/org/vismayb/mocha/Loggable.java
+++ src/org/vismayb/mocha/Loggable.java
@@ -2,6 +2,8 @@ package org.vismayb.mocha;
 
 /**
  * To be implemented by classes that need information logged at runtime, but do not require a string representation
+ * @implNote This has been created in Java in order to make use of the {@code default} keyword until GUI code gets
+ * converted to Kotlin as well.
  */
 public interface Loggable {
     /**
